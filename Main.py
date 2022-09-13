7b8b91fb29213ecc332e1413223469e9a5fa94ba
URK21CO2007 <110015351+URK21CO2007@users.noreply.github.com>
2022-09-13 12:09:02 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
 from typing import List
 
 def selectionSort(array, size) -> List[int]:
-  # Write your code here
+  for i in range(1, len(arr))
+    temp=arr[i]
+    j=i-1
+    while j>=0 and temop<arr[i]
+    arr[j+1]=arr[j]
+    j=j-1
+    arr[j+1]=temp
 
 # Do not change the following code
 input_data = input()
